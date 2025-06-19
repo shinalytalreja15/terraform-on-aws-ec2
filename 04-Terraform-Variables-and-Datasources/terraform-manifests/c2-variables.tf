0dcf8f14740aeca428e3a9768d6726871d350cdc
shinalytalreja15 <55690532+shinalytalreja15@users.noreply.github.com>
2025-06-19 22:25:40 +0100

    Update c2-variables.tf

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
@@ -1,4 +1,13 @@
 # Input Variables
+# mastertest
+variable "mastertest" {
+  description = "mastertest"
+  type = string
+  default = "us-east-1"
+}
+
+
+
 # AWS Region
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
