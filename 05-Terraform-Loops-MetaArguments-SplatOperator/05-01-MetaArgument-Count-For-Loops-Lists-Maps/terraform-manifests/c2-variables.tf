e8814ec6a4cb2dba7a17ab70a1bb367e256939ee
shinalytalreja15 <55690532+shinalytalreja15@users.noreply.github.com>
2025-06-19 22:18:17 +0100

    Update c2-variables.tf

--- 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c2-variables.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/05-01-MetaArgument-Count-For-Loops-Lists-Maps/terraform-manifests/c2-variables.tf
@@ -1,4 +1,13 @@
 # Input Variables
+# test
+variable "test" {
+  description = "test"
+  type = string
+  default = "us-east-1"
+}
+
+
+
 # AWS Region
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
